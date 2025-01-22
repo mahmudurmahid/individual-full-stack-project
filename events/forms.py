18eb1c847846ed6d5e10b68f5f1e00067639c5ad
Mahmudur Mahid <m.mahid@icloud.com>
2025-01-22 19:41:34 +0000

    fixed login error

--- events/forms.py
+++ events/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate  # Import authenticate
 from .models import Event, Profile
 import re
 
--- music_booking/backends.py
+++ music_booking/backends.py
@@ -1,22 +1,14 @@
 from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.models import User
 
 class CustomAuthBackend(BaseBackend):
-    """
-    Custom authentication backend to log in users using email and password.
-    """
-
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            # Find user by email
-            user = User.objects.get(email=username)
-            if user.check_password(password):  # Check password validity
+            user = User.objects.get(email=username)  # Authenticate using email
+            if user.check_password(password):
                 return user
         except User.DoesNotExist:
             return None
-        return None
 
     def get_user(self, user_id):
         try:
